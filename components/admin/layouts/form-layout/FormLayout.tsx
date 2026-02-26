c4e266703fc186a59eb656269777541b435b8d12
9567828 <kimssi9567@gmail.com>
2026-02-26 18:31:47 +0900

    Fix: 커밋테스트

--- components/admin/layouts/form-layout/FormLayout.tsx
+++ components/admin/layouts/form-layout/FormLayout.tsx
@@ -1,6 +1,5 @@
-import { FormHTMLAttributes, useState } from "react";
+import { FormHTMLAttributes } from "react";
 import style from "./form.module.scss";
-import { useSideBarStateStore } from "@/hooks/store/useSideBarStateStore";
 import { FormType } from "@/utils/propType";
 import FormFooter from "../form-footer/FormFooter";
 
@@ -14,11 +13,27 @@ interface IForm extends FormHTMLAttributes<HTMLFormElement> {
   children: React.ReactNode;
 }
 
-export default function FormLayout({ mode, variants, onDelete, onBack, onReset, onMoveEdit, children, ...props }: IForm) {
+export default function FormLayout({
+  mode,
+  variants,
+  onDelete,
+  onBack,
+  onReset,
+  onMoveEdit,
+  children,
+  ...props
+}: IForm) {
   return (
     <form {...props} className={style[variants]}>
       {children}
-      <FormFooter formId={props.id!} mode={mode} onBack={onBack} onClick={onMoveEdit} onDelete={onDelete} onReset={onReset} />
+      <FormFooter
+        formId={props.id!}
+        mode={mode}
+        onBack={onBack}
+        onClick={onMoveEdit}
+        onDelete={onDelete}
+        onReset={onReset}
+      />
     </form>
   );
 }
